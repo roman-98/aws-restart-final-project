52df9688881f5dca60aef645e0934fec465625b6
Roman Strypa <romanstripa@gmail.com>
2024-10-25 16:23:44 +0100

    terraform-destroy

--- terraform.tf
+++ terraform.tf
@@ -31,7 +31,10 @@ resource "aws_s3_bucket_policy" "website_policy" {
       {
         Effect    = "Allow"
         Principal = "*"
-        Action    = "s3:GetObject"
+        Action    = [
+          "s3:GetObject",
+          "s3:DeleteObject"
+        ]
         Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
       }
     ]
