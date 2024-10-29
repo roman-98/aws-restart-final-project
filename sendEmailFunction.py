e00499f11d25608ff1a5b8a3ad3d1e3b8a36a6bc
Roman Strypa <romanstripa@gmail.com>
2024-10-29 10:53:42 +0000

    terraform-apply

--- sendEmailFunction.py
+++ sendEmailFunction.py
@@ -1,28 +1,22 @@
-import boto3
 import json
+import boto3
 import os
 
 sns_client = boto3.client('sns')
+sns_topic_arn = os.environ['SNS_TOPIC_ARN']
 
 def lambda_handler(event, context):
-    try:
-        body = json.loads(event['body'])
-        message = body.get("message", "")
-        subject = "New Message from Website"
+    body = json.loads(event.get('body', '{}'))
+    message = body.get("message", "")
+    subject = "New Message from Website"
 
-        response = sns_client.publish(
-            TopicArn=os.environ['SNS_TOPIC_ARN'],
-            Message=message,
-            Subject=subject
-        )
+    response = sns_client.publish(
+        TopicArn=sns_topic_arn,
+        Message=message,
+        Subject=subject
+    )
 
-        return {
-            'statusCode': 200,
-            'body': json.dumps('Message sent successfully')
-        }
-    except Exception as e:
-        print(f"Error: {e}")
-        return {
-            'statusCode': 500,
-            'body': json.dumps('Error sending message')
-        }
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Message sent successfully')
+    }
--- terraform.tf
+++ terraform.tf
@@ -49,3 +49,67 @@ resource "aws_s3_bucket_policy" "website_policy" {
 output "website_url" {
   value = aws_s3_bucket.website_bucket.website_domain
 }
+
+resource "aws_sns_topic" "website_messages" {
+  name = "websiteMessagesTopic"
+}
+
+resource "aws_sns_topic_subscription" "email_subscription" {
+  topic_arn = aws_sns_topic.website_messages.arn
+  protocol  = "email"
+  endpoint  = "romanstripa@gmail.com"
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_sns_publish_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambda_sns_policy" {
+  name        = "lambda_sns_publish_policy"
+  description = "IAM policy for Lambda to publish messages to SNS"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "sns:Publish",
+        Resource = aws_sns_topic.website_messages.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_sns_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_sns_policy.arn
+}
+
+resource "aws_lambda_function" "send_message" {
+  function_name = "sendMessageFunction"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
+  filename      = "sendEmailFunction.py" 
+
+  environment {
+    variables = {
+      SNS_TOPIC_ARN = aws_sns_topic.website_messages.arn
+    }
+  }
+
+  source_code_hash = filebase64sha256("sendEmailFunction.py")
+}
